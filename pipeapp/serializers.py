f08412042085d640729fff8e066a39f3a1d230c8
Habeebullahi Lawal <h.lawal@toybethsystems.com>
2024-08-19 12:27:54 +0100

    staginghgsahtddde

--- pipeapp/serializers.py
+++ pipeapp/serializers.py
@@ -98,6 +98,50 @@ class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
+
+from rest_framework import serializers
+from .models import PipelineRoute, State, PipelineFault
+
+class PipelineRouteSerializer(serializers.ModelSerializer):
+    state = serializers.CharField()  # Accept state as a char field (name)
+
+    class Meta:
+        model = PipelineRoute
+        fields = ['id', 'name', 'state', 'coordinates']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        state_name = validated_data.pop('state')
+        try:
+            state = State.objects.get(name=state_name)
+        except State.DoesNotExist:
+            raise serializers.ValidationError({'state': f"State '{state_name}' does not exist."})
+
+        pipeline_route = PipelineRoute.objects.create(state=state, **validated_data)
+        return pipeline_route
+
+    def update(self, instance, validated_data):
+        state_name = validated_data.pop('state', None)
+        if state_name:
+            try:
+                state = State.objects.get(name=state_name)
+            except State.DoesNotExist:
+                raise serializers.ValidationError({'state': f"State '{state_name}' does not exist."})
+            instance.state = state
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+from rest_framework import serializers
+from .models import PipelineRoute, PipelineFault, State
+
+# Serializer for individual faults
+class PipelineFaultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PipelineFault
+        fields = ['fault_coordinates', 'description', 'reported_at', 'status']
+
 # Serializer for PipelineRoute, including faults and computed status
 class PipelineRouteAndFaultSerializer(serializers.ModelSerializer):
     state = serializers.CharField()  # Accept state as a char field (name)
