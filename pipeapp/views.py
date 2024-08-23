14986923497a9e30812d5bf34e753068c03dce32
Habeebullahi Lawal <h.lawal@toybethsystems.com>
2024-08-23 12:21:53 +0100

    nmafgffNANAMMAAaaaamma

--- pipeapp/views.py
+++ pipeapp/views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
 from django.contrib.auth import logout as django_logout
-from .serializers import UserSerializer, LoginSerializer, PipelineRouteAndFaultSerializer,UserDetailSerializer
+from .serializers import UserSerializer, LoginSerializer, PipelineRouteAndFaultSerializer, UserDetailSerializer
 from .models import PipelineRoute, Profile
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
 User = get_user_model()
 
 class IsHigherRole(permissions.BasePermission):
@@ -34,27 +35,19 @@ class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated, IsHigherRole]  # Ensure user is logged in and has permission
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
 
     def perform_create(self, serializer):
         serializer.save()
 
-from rest_framework.permissions import AllowAny
-from django.contrib.auth import authenticate, login as django_login
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import LoginSerializer, UserDetailSerializer
-from drf_yasg.utils import swagger_auto_schema
-
 class UserLoginView(APIView):
     authentication_classes = []
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.AllowAny]
 
     @swagger_auto_schema(request_body=LoginSerializer)
     def post(self, request, *args, **kwargs):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            # Access the user object from the validated data
             user = serializer.validated_data['user']
 
             # Log the user in
@@ -69,10 +62,9 @@ class UserLoginView(APIView):
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class PipelineRouteAndFaultViewSet(viewsets.ModelViewSet):
     serializer_class = PipelineRouteAndFaultSerializer
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
@@ -93,12 +85,10 @@ class PipelineRouteAndFaultViewSet(viewsets.ModelViewSet):
         
         return queryset.none()  # Fallback to no data if no role matches
 
-
 class UserLogoutView(APIView):
     authentication_classes = []
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         django_logout(request)
         return Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
-
