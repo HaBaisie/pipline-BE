213749bdabfbf6de293251530e1fbf028dda78e8
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-08-05 12:47:50 +0100

    Update urls.py

--- pipeapp/urls.py
+++ pipeapp/urls.py
@@ -18,10 +18,10 @@ router.register(r'pipeline-routes-viewset', PipelineRouteAndFaultViewSet, basena
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    # path('pipeline-routes/', PipelineRouteListCreateView.as_view(), name='pipeline-routes-list-create'),
-    # path('pipeline-routes/<int:pk>/', PipelineRouteDetailView.as_view(), name='pipeline-routes-detail'),
-    # path('pipeline-faults/', PipelineFaultListCreateView.as_view(), name='pipeline-faults-list-create'),
-    # path('pipeline-faults/<int:pk>/', PipelineFaultDetailView.as_view(), name='pipeline-faults-detail'),
+    path('pipeline-routes/', PipelineRouteListCreateView.as_view(), name='pipeline-routes-list-create'),
+    path('pipeline-routes/<int:pk>/', PipelineRouteDetailView.as_view(), name='pipeline-routes-detail'),
+    path('pipeline-faults/', PipelineFaultListCreateView.as_view(), name='pipeline-faults-list-create'),
+    path('pipeline-faults/<int:pk>/', PipelineFaultDetailView.as_view(), name='pipeline-faults-detail'),
     path('', include(router.urls)),  # Include the router's URLs
     path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
